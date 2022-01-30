48599d50cde059d4ed3f7d3a3edbf7132db1c4f7
Alexis <alexisrobin21@gmail.com>
2022-01-30 19:36:19 +0100

    Init project structure
    - Added main components files
    - Added main styles files

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
+import Header from './components/Header';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      
+    <div>
+      <Header />
     </div>
   );
-}
+};
 
 export default App;
+
